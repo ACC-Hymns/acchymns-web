1771c2d3866e1849f2809e18298f8d648b6f8c5d
Nomalah <36043275+Nomalah@users.noreply.github.com>
2022-08-18 01:22:20 -0400

    Add joining code

---
+++ books/index.js
@@ -0,0 +1,22 @@
+import * as GH from "./GH/index.json";
+import * as HG from "./HG/index.json";
+import * as HZ from "./HZ/index.json";
+import * as JH from "./JH/index.json";
+import * as PC from "./PC/index.json";
+import * as ZG from "./ZG/index.json";
+import * as ZH from "./ZH/index.json";
+
+const SONG_BOOKS = [
+    GH,
+    ZH,
+    JH,
+    // Add-on books
+    HG,
+    HZ,
+    PC,
+    ZG
+]
+
+export {
+    SONG_BOOKS
+}
