142fe2018f3372f6751e7086eb77d3c90ade208b
Nomalah <36043275+Nomalah@users.noreply.github.com>
2022-08-19 00:28:22 -0400

    Bookmarks work, going back on bookmark page resets search

--- js/bookmark-button.js
+++ js/bookmark-button.js
@@ -1,42 +1,34 @@
-import { getFullBook } from "./helpers.js";
-
-function isBookmarked(number, book) {
-    let bookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
-    if(bookmarks == null)
-        return false;
-
-    for(let bookmark of bookmarks) {
-        if(bookmark.number == number && bookmark.bookShort == book) {
-            return true;
-        }
-    }
-    return false;
-}
-
 const bookmarkButton = document.getElementById('bookmarkButton');
 const bookmarkIcon = document.getElementById('bookmarkIcon');
+const urlParams = new URLSearchParams(window.location.search);
+const bookName = urlParams.get("book");
+const songNum = urlParams.get("song");
 
 bookmarkButton.addEventListener('click', (e) => {
     let bookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
     if (bookmarks == null)
         bookmarks = [];
-    let index = bookmarks.findIndex(bookmark => bookmark.number == songNum && bookmark.bookShort == bookName)
+    let index = bookmarks.findIndex(bookmark => bookmark.book == bookName && bookmark.song == songNum);
     if (index != -1) {
         bookmarks.splice(index, 1);
         bookmarkIcon.setAttribute('name', "bookmark-outline");
     } else {
-        bookmarks.push(
-            {"number": songNum, "bookShort": bookName, "title": songTitle, "book": getFullBook(bookName)}
-        );
+        bookmarks.push({"book": bookName, "song": songNum});
         bookmarkIcon.setAttribute('name', "bookmark");
     }
     
     window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 });
 
+function getBookmarkIndex(number, book) {
+    let bookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
+    if(bookmarks == null)
+        return -1;
+    return bookmarks.findIndex(bookmark => bookmark.book == book && bookmark.song == number);
+}
 
 // Figure out on initialization if bookmarked
-if(isBookmarked(songNum, bookName)) {
+if(getBookmarkIndex(songNum, bookName) != -1) {
     bookmarkIcon.setAttribute('name', "bookmark");
 } else {
     bookmarkIcon.setAttribute('name', "bookmark-outline");
