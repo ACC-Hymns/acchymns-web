a13905b3f5b467ad593af4040d27689f11483241
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-01-14 16:52:06 -0500

    Disable Topical index

--- js/topical-index-button.js
+++ js/topical-index-button.js
@@ -1,13 +1,13 @@
 import { getBookIndex } from "/books/index.js";
 
-const topicalIndexButton = document.getElementById('topicalIndexButton');
-topicalIndexButton.href = `topical-index.html${window.location.search}`;
+// const topicalIndexButton = document.getElementById('topicalIndexButton');
+// topicalIndexButton.href = `topical-index.html${window.location.search}`;
 
-(async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const bookName = urlParams.get("book");
-    if (await getBookIndex(bookName) != null) {
-        const topicalIndexIcon = document.getElementById('topicalIndexIcon');
-        topicalIndexIcon.classList.remove("hidden");
-    }
-})();
+// (async () => {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const bookName = urlParams.get("book");
+//     if (await getBookIndex(bookName) != null) {
+//         const topicalIndexIcon = document.getElementById('topicalIndexIcon');
+//         topicalIndexIcon.classList.remove("hidden");
+//     }
+// })();
