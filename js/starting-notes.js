6dba592db685dbd7beb3c5dba4c1efd45d7bebd9
Christoffyw <weinhardtchristopher@gmail.com>
2023-01-16 17:12:03 -0500

    Rework Tone.Sampler to use XHR

--- js/starting-notes.js
+++ js/starting-notes.js
@@ -16,30 +16,51 @@ if (bookName != null && songNum != null){
     })();
 }
 
-const sampler = new Tone.Sampler({
-	urls: {
-		"A2": "A2.mp3",
-		"C3": "C3.mp3",
-		"A3": "A3.mp3",
-		"C4": "C4.mp3",
-		"A4": "A4.mp3",
-		"C5": "C5.mp3"
-	},
-	baseUrl: "/assets/notes/",
-}).toDestination();
-
-
-startingNotesButton.addEventListener('mousedown', (e) => {
-    var interval = (window.localStorage.getItem("playbackInterval") == undefined) ? 0.25 : parseFloat(window.localStorage.getItem("playbackInterval"));
-    var duration = (window.localStorage.getItem("playbackDuration") == undefined) ? 3 : parseFloat(window.localStorage.getItem("playbackDuration"));
-    if (book_song_metadata != null && book_song_metadata[songNum]["notes"] != null) {
-        if(window.localStorage.getItem("staggered") == "true" || window.localStorage.getItem("staggered") == undefined) {
-            for(var i = (book_song_metadata[songNum]["notes"].length); i > 0; i--) {
-                sampler.triggerAttack(book_song_metadata[songNum]["notes"][i - 1], Tone.now() + (interval * (book_song_metadata[songNum]["notes"].length - i)));
+function finished() {
+    const sampler = new Tone.Sampler({
+        urls: {
+            "A2": blob_A2,
+            "C3": blob_C3,
+            "A3": blob_A3,
+            "C4": blob_C4,
+            "A4": blob_A4,
+            "C5": blob_C5
+        },
+        onload: () => {
+            console.log("playing");
+            var interval = (window.localStorage.getItem("playbackInterval") == undefined) ? 0.25 : parseFloat(window.localStorage.getItem("playbackInterval"));
+            var duration = (window.localStorage.getItem("playbackDuration") == undefined) ? 3 : parseFloat(window.localStorage.getItem("playbackDuration"));
+            if (book_song_metadata != null && book_song_metadata[songNum]["notes"] != null) {
+                if(window.localStorage.getItem("staggered") == "true" || window.localStorage.getItem("staggered") == undefined) {
+                    for(var i = (book_song_metadata[songNum]["notes"].length); i > 0; i--) {
+                        sampler.triggerAttack(book_song_metadata[songNum]["notes"][i - 1], Tone.now() + (interval * (book_song_metadata[songNum]["notes"].length - i)));
+                    }
+                } else {
+                    sampler.triggerAttack(book_song_metadata[songNum]["notes"], Tone.now());
+                }
+                sampler.triggerRelease(book_song_metadata[songNum]["notes"], Tone.now() + duration + (interval * book_song_metadata[songNum]["notes"].length));
             }
-        } else {
-            sampler.triggerAttack(book_song_metadata[songNum]["notes"], Tone.now());
         }
-        sampler.triggerRelease(book_song_metadata[songNum]["notes"], Tone.now() + duration + (interval * book_song_metadata[songNum]["notes"].length));
+
+    }).toDestination();
+}
+
+startingNotesButton.addEventListener('mousedown', async (e) => {
+    await Tone.start();
+    let index = ["A2", "C3", "A3", "C4", "A4", "C5"];
+    for (var i = 0; i < index.length; i++) {
+
+        var url = "/assets/notes/" + index[i] + ".mp3";
+        console.log(url);
+        let request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.responseType = 'blob';
+        request.customdata = {note: index[i], index: index, iteration: i};
+        request.onload = function(){
+            window['blob_' + this.customdata.note] = URL.createObjectURL(this.response);
+            if(this.customdata.iteration == (this.customdata.index.length - 1))
+                finished();
+        };
+        request.send();
     }
 });
