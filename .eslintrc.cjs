c89cd070bc5b2f98c77a4f646bea0094e7b6b951
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-03-29 20:58:21 -0400

    Fix intellisense

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,15 @@ module.exports = {
     ],
     ignorePatterns: ["ios/*", "android/*"],
     rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                // Override all options of `prettier` here
+                // @see https://prettier.io/docs/en/options.html
+                tabWidth: 4,
+                printWidth: 120,
+            },
+        ],
         // Ignore all unused variables that start with an underscore
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": [
