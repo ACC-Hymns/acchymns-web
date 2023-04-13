dcce2aa690cd69abf102072b6264341f2670e838
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-04-13 17:55:58 -0400

    Update and fix build

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,24 +1,16 @@
-import { ref, computed } from "vue";
-import { defineStore } from "pinia";
-import type { BookSummary } from "@/scripts/types";
+// import { ref, computed } from "vue";
+// import { defineStore } from "pinia";
+// import type { BookSummary } from "@/scripts/types";
 
-export const useCounterStore = defineStore("counter", () => {
-    const count = ref(0);
-    const doubleCount = computed(() => count.value * 2);
-    function increment() {
-        count.value++;
-    }
+// export const useBookMetadataStore = defineStore("book_metadata", () => {
+//     const last_fetched_time = ref(new Date());
+//     const cached = ref(0);
+//     const doubleCount = computed(() => count.value * 2);
+//     function increment() {
+//         count.value++;
+//     }
 
-    return { count, doubleCount, increment };
-});
+//     return { last_fetched_time, doubleCount, increment };
+// });
 
-export const useBookMetadataStore = defineStore("book_metadata", () => {
-    const last_fetched_time = ref(new Date());
-    const cached = ref(0);
-    const doubleCount = computed(() => count.value * 2);
-    function increment() {
-        count.value++;
-    }
-
-    return { last_fetched_time, doubleCount, increment };
-});
+export {};
