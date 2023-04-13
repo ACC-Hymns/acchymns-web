b822fedf209b8303fc8901bc7d4e10529352a85b
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-04-13 08:10:30 -0400

    Start Cached store

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,5 +1,6 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import type { BookSummary } from "@/scripts/types";
 
 export const useCounterStore = defineStore("counter", () => {
     const count = ref(0);
@@ -9,4 +10,15 @@ export const useCounterStore = defineStore("counter", () => {
     }
 
     return { count, doubleCount, increment };
+});
+
+export const useBookMetadataStore = defineStore("book_metadata", () => {
+    const last_fetched_time = ref(new Date());
+    const cached = ref(0);
+    const doubleCount = computed(() => count.value * 2);
+    function increment() {
+        count.value++;
+    }
+
+    return { last_fetched_time, doubleCount, increment };
 });
