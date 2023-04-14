eb87f55efc0ec97815fd212b3c82f6e212165484
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-04-13 23:17:10 -0400

    test loading with base_url

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
         },
-    },
+        base: env.VITE_BASE_URL,
+    };
 });
