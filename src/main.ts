38d1015a243991522d2921c782170c59aa93ac34
Christoffyw <weinhardtchristopher@gmail.com>
2023-04-15 23:04:39 -0400

    fixed insets and removed unnecessary package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { SafeAreaController, SafeArea } from '@aashu-dubey/capacitor-statusbar-safe-area';
 // import { createPinia } from "pinia";
 
 import App from "./App.vue";
@@ -11,4 +10,3 @@ const app = createApp(App);
 app.use(router);
 
 app.mount("#app");
-SafeAreaController.injectCSSVariables();
