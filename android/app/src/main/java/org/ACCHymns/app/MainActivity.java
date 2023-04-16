ee449c94659ae083b2728bf14bad4eae5992d8d9
Christoffyw <weinhardtchristopher@gmail.com>
2023-04-16 00:34:02 -0400

    Android scroll bounce + small fixes
    - Fixed ZH #47 starting notes
    - Fixed CSS in search and bookmarks

--- android/app/src/main/java/org/ACCHymns/app/MainActivity.java
+++ android/app/src/main/java/org/ACCHymns/app/MainActivity.java
@@ -1,5 +1,15 @@
 package org.ACCHymns.app;
 
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+     @Override
+  public void onStart() {
+    super.onStart();
+    // Disable the rubber-band over-scroll effect that causes the app UI to get stretched.
+    WebView v = getBridge().getWebView();
+    v.setOverScrollMode(v.OVER_SCROLL_NEVER);
+  }
+}
