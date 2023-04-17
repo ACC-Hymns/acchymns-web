53a76e8589811e25b070d1d61c6a5b949f5b0e88
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-04-16 20:01:05 -0400

    Format

--- android/app/src/main/java/org/ACCHymns/app/MainActivity.java
+++ android/app/src/main/java/org/ACCHymns/app/MainActivity.java
@@ -5,11 +5,11 @@ import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-     @Override
-  public void onStart() {
-    super.onStart();
-    // Disable the rubber-band over-scroll effect that causes the app UI to get stretched.
-    WebView v = getBridge().getWebView();
-    v.setOverScrollMode(v.OVER_SCROLL_NEVER);
-  }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Disable the rubber-band over-scroll effect that causes the app UI to get stretched.
+        WebView v = getBridge().getWebView();
+        v.setOverScrollMode(v.OVER_SCROLL_NEVER);
+    }
 }
