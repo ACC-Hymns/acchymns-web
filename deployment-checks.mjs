da84dfdd46b70093a01ae5c1f5ab27bdbd9615ad
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-07-12 01:17:13 -0400

    feat: add checks for blank notes on songs

--- deployment-checks.mjs
+++ deployment-checks.mjs
@@ -5,27 +5,50 @@
 // that every reference in the book's songs.json file has
 // a corresponding image
 
+import { execSync } from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 import process from "node:process";
 
+const branch_name = execSync("git branch --show-current").toString().trimEnd();
+const is_production_or_staging = branch_name == "staging" || branch_name == "main";
+
 const books = fs.readdirSync("public/books");
 const missing_images = [];
-const missing_references = [];
+const missing_number = [];
 for (const book of books) {
     console.log("Checking", book);
-    const song_references = Object.keys(JSON.parse(fs.readFileSync(path.join("public/books", book, "songs.json"))));
+
+    // Check missing image/reference mappings
+    const song_references = JSON.parse(fs.readFileSync(path.join("public/books", book, "songs.json")));
+    const song_numbers = Object.keys(song_references);
     const image_titles = fs.readdirSync(path.join("public/books", book, "songs")).map(file => file.split(".")[0]);
-    for (const song_reference of song_references) {
-        if (!image_titles.includes(song_reference)) {
-            console.log("Missing image for", song_reference);
-            missing_images.push(book + " - " + song_reference);
+    for (const song_number of song_numbers) {
+        if (!image_titles.includes(song_number)) {
+            console.log("Missing image for", song_number);
+            missing_images.push(book + " - " + song_number);
         }
     }
     for (const image_title of image_titles) {
-        if (!song_references.includes(image_title)) {
-            console.log("Missing song reference for", image_title);
-            missing_references.push(book + " - " + image_title);
+        if (!song_numbers.includes(image_title)) {
+            console.log("Missing song number for", image_title);
+            missing_number.push(book + " - " + image_title);
+        }
+    }
+
+    // Check for missing references (not a production issue, but we should know about it so we can add missing songs)
+    const song_numbers_int = song_numbers.map(reference => parseInt(reference)); // Songs ending with a/b should still be converted
+    const max_song_number = Math.max(...song_numbers_int);
+    for (let i = 1; i <= max_song_number; i++) {
+        if (!song_numbers_int.includes(i)) {
+            console.log(`Missing song #${i}`);
+        }
+    }
+
+    // Check for blank notes (is a production issue, so we should know about it so we can add missing songs)
+    for (const [song_number, song_reference] of Object.entries(song_references)) {
+        if (song_reference["notes"] != undefined && song_reference["notes"].join("").trim() == "") {
+            console.log(`Blank notes for song #${song_number}`);
         }
     }
 }
@@ -35,12 +58,13 @@ if (missing_images.length > 0) {
     console.log(missing_images.join("\n"));
 }
 
-if (missing_references.length > 0) {
+if (missing_number.length > 0) {
     console.log("Missing references:");
-    console.log(missing_references.join("\n"));
+    console.log(missing_number.join("\n"));
 }
 
-if (missing_images.length > 0 || missing_references.length > 0) {
+// Fail if there are missing references or images
+if (missing_images.length > 0 || missing_number.length > 0) {
     process.exit(1);
 }
 process.exit(0);
