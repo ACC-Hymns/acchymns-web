2dec808f4c829acde1c5570a6cb4a88a4b13858e
Christopher Weinhardt <55860414+Christoffyw@users.noreply.github.com>
2023-07-15 12:39:50 -0400

    Update migrate.ts

--- src/scripts/migrate.ts
+++ src/scripts/migrate.ts
@@ -7,9 +7,8 @@ export async function migrate() {
         return;
     }
 
-    // TODO - FIX MIGRATION
     // Pre-2.0.0, as after this version, we always set the AppVersion.
-    /*if (previous_semver_version.value == null) {
+    if (previous_semver_version.value == null) {
         // Migrate from 1.X.Y to 2.0.0
         // All that's required to migrate, is to move bookmark data from localStorage to Capacitor Storage.
         // and to change from { book: string, song: string } to { book: string, number: string }
@@ -24,5 +23,6 @@ export async function migrate() {
             }
         }
         await Preferences.set({ key: "bookmarks", value: JSON.stringify(new_bookmarks) });
-    }*/
+    }
+    await Preferences.set({ key: “AppVersion”, value: current_semver_version });
 }
