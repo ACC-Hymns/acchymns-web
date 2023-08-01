74c2f977f93c708ee71b429e83727b9b18c7d120
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-08-01 11:15:36 -0400

    feat: initial SEO and meta tags for books

---
+++ seo-deployment.mjs
@@ -0,0 +1,100 @@
+// This script is for generating index.html files
+// with meta tags filled out for each page for SEO
+// This script should be run before deployment for web (and really only web)
+
+import fs from "node:fs";
+import { parse } from "node-html-parser";
+import dotenv from "dotenv";
+dotenv.config();
+import process from "node:process";
+if (process.env.VITE_BASE_URL == undefined) {
+    process.env.VITE_BASE_URL = "/";
+}
+
+// Assume you're looking at the preview site
+if (process.env.SEO_URL == undefined) {
+    process.env.SEO_URL = "https://localhost:4173";
+}
+
+// Site navigation
+// / <- home
+// /selection/* <- book selection
+// /topical/* <- topical & alphabetical index
+// /search <- search
+// /bookmarks <- bookmarks
+// /settings <- settings
+// /settings/about <- about
+// /settings/about/attributions <- attributions
+// /settings/about/changelog <- changelog
+// /settings/help <- help
+// /settings/options <- options
+// /settings/import <- import books
+// /display/*/* <- display song
+
+function generateMetaTags(title, description, url_rel, image_rel, type) {
+    return `
+    <meta property="og:title" content="${title}" />
+    <meta property="og:description" content="${description}" />
+    <meta property="og:url" content="https://acchymns.app/${url_rel}" />
+    <meta property="og:image" content="${process.env.SEO_URL}${process.env.VITE_BASE_URL}${image_rel}" />
+    <meta property="og:type" content="${type}" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:site_name" content="ACCHymns" />
+    `;
+}
+
+const dist_dir = "dist";
+const index_html = fs.readFileSync(`${dist_dir}/index.html`, "utf-8");
+
+// Generate index.html for all pages
+
+// / <- home
+{
+    const dup = parse(index_html);
+    const head = dup.getElementsByTagName("head")[0];
+    head.insertAdjacentHTML("beforeend", generateMetaTags("ACCHymns", "A website for the Apostolic Christian Church of America hymnbook", "", "assets/icons/180x180.png", "website"));
+    // fs.writeFileSync(`${dist_dir}/index.html`, dup.toString());
+}
+
+// /selection/* <- book selection
+const books = ["ZH", "GH", "JH", "HG", "CH", "HZ", "ZG", "ZGE", "ZHJ", "ZHSP", "ZHG", "ZHH", "ZHR", "HS", "PC"];
+for (const book of books) {
+    const dup = parse(index_html);
+    const head = dup.getElementsByTagName("head")[0];
+    const summary = JSON.parse(fs.readFileSync(`public/books/${book}/summary.json`, "utf-8"));
+    head.insertAdjacentHTML(
+        "beforeend",
+        generateMetaTags(
+            summary.name.long,
+            `${summary.name.medium} available online and for download via the app store`,
+            `selection/${book}`,
+            `assets/icons/180x180.png`, // We can replace this with some kind of custom image for each book
+            "music.album"
+        )
+    );
+    fs.mkdirSync(`${dist_dir}/selection/${book}`, { recursive: true });
+    fs.writeFileSync(`${dist_dir}/selection/${book}/index.html`, dup.toString());
+}
+
+// /display/*/* <- display song
+for (const book of books) {
+    const summary = JSON.parse(fs.readFileSync(`public/books/${book}/summary.json`, "utf-8"));
+    const song_list = JSON.parse(fs.readFileSync(`public/books/${book}/songs.json`, "utf-8"));
+    for (const song of Object.keys(song_list)) {
+        const dup = parse(index_html);
+        const head = dup.getElementsByTagName("head")[0];
+
+        head.insertAdjacentHTML(
+            "beforeend",
+            generateMetaTags(
+                song_list[song].title,
+                `#${song} from ${summary.name.medium} available online and for download via the app store`,
+                `display/${book}/${song}`,
+                `assets/icons/180x180.png`, // We can replace this with some kind of custom image for each book
+                "music.album"
+            )
+        );
+        fs.mkdirSync(`${dist_dir}/display/${book}/${song}`, { recursive: true });
+        fs.writeFileSync(`${dist_dir}/display/${book}/${song}/index.html`, dup.toString());
+    }
+}
