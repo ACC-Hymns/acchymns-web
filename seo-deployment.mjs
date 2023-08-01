74a3840603bd8f193ae5d448bb3c5c16d89244bc
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-08-01 12:50:17 -0400

    feat: more SEO, slightly faster?

--- seo-deployment.mjs
+++ seo-deployment.mjs
@@ -52,12 +52,13 @@ const index_html = fs.readFileSync(`${dist_dir}/index.html`, "utf-8");
 {
     const dup = parse(index_html);
     const head = dup.getElementsByTagName("head")[0];
-    head.insertAdjacentHTML("beforeend", generateMetaTags("ACCHymns", "A website for the Apostolic Christian Church of America hymnbook", "", "assets/icons/180x180.png", "website"));
-    // fs.writeFileSync(`${dist_dir}/index.html`, dup.toString());
+    head.insertAdjacentHTML("beforeend", generateMetaTags("ACC Hymns", "A website for hymnbooks used by various ACC churches", "", "assets/icons/180x180.png", "website"));
+    fs.writeFileSync(`${dist_dir}/index.html`, dup.toString());
 }
 
 // /selection/* <- book selection
-const books = ["ZH", "GH", "JH", "HG", "CH", "HZ", "ZG", "ZGE", "ZHJ", "ZHSP", "ZHG", "ZHH", "ZHR", "HS", "PC"];
+const books = ["ZH", "GH", "JH", "HG", "CH", "HZ", "ZG", "ZGE", "ZHJ", "ZHSP", "ZHG", "ZHH", "ZHR", "HS", "PC", "ARF", "ARFR"];
+fs.mkdirSync(`${dist_dir}/selection`, { recursive: true });
 for (const book of books) {
     const dup = parse(index_html);
     const head = dup.getElementsByTagName("head")[0];
@@ -72,14 +73,14 @@ for (const book of books) {
             "music.album"
         )
     );
-    fs.mkdirSync(`${dist_dir}/selection/${book}`, { recursive: true });
-    fs.writeFileSync(`${dist_dir}/selection/${book}/index.html`, dup.toString());
+    fs.writeFileSync(`${dist_dir}/selection/${book}`, dup.toString());
 }
 
 // /display/*/* <- display song
 for (const book of books) {
     const summary = JSON.parse(fs.readFileSync(`public/books/${book}/summary.json`, "utf-8"));
     const song_list = JSON.parse(fs.readFileSync(`public/books/${book}/songs.json`, "utf-8"));
+    fs.mkdirSync(`${dist_dir}/display/${book}`, { recursive: true });
     for (const song of Object.keys(song_list)) {
         const dup = parse(index_html);
         const head = dup.getElementsByTagName("head")[0];
@@ -91,10 +92,41 @@ for (const book of books) {
                 `#${song} from ${summary.name.medium} available online and for download via the app store`,
                 `display/${book}/${song}`,
                 `assets/icons/180x180.png`, // We can replace this with some kind of custom image for each book
-                "music.album"
+                "music.song"
             )
         );
-        fs.mkdirSync(`${dist_dir}/display/${book}/${song}`, { recursive: true });
-        fs.writeFileSync(`${dist_dir}/display/${book}/${song}/index.html`, dup.toString());
+        fs.writeFileSync(`${dist_dir}/display/${book}/${song}`, dup.toString());
     }
+}
+
+// /search <- search
+{
+    const dup = parse(index_html);
+    const head = dup.getElementsByTagName("head")[0];
+    head.insertAdjacentHTML(
+        "beforeend",
+        generateMetaTags("ACC Hymns Search", "Search for all of your favorite hymns from the Zion's Harp, Gospel Hymns, Junior Hymnal, and many more!", "search", "assets/icons/180x180.png", "website")
+    );
+    fs.writeFileSync(`${dist_dir}/search`, dup.toString());
+}
+
+// /bookmarks <- bookmarks
+{
+    const dup = parse(index_html);
+    const head = dup.getElementsByTagName("head")[0];
+    head.insertAdjacentHTML("beforeend", generateMetaTags("ACC Hymns Bookmarks", "View your bookmarks from the ACC Hymns app", "bookmarks", "assets/icons/180x180.png", "website"));
+    fs.writeFileSync(`${dist_dir}/bookmarks`, dup.toString());
+}
+
+// /topical/* <- topical & alphabetical index
+fs.mkdirSync(`${dist_dir}/topical`, { recursive: true });
+for (const book of books) {
+    const dup = parse(index_html);
+    const head = dup.getElementsByTagName("head")[0];
+    const summary = JSON.parse(fs.readFileSync(`public/books/${book}/summary.json`, "utf-8"));
+    head.insertAdjacentHTML(
+        "beforeend",
+        generateMetaTags(`${summary.name.long} Topical Index`, `View the topical and alphabetical index for ${summary.name.medium}`, `topical/${book}`, "assets/icons/180x180.png", "music.playlist")
+    );
+    fs.writeFileSync(`${dist_dir}/topical/${book}`, dup.toString());
 }
