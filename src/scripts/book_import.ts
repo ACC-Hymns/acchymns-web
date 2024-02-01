9ceb5855241d161785d4e28f5606fdc4e8703769
Christoffyw <weinhardtchristopher@gmail.com>
2024-02-01 18:45:34 -0500

    Add book download method

--- src/scripts/book_import.ts
+++ src/scripts/book_import.ts
@@ -1,11 +1,72 @@
 import { fetchCachedJSON } from "@/composables/cached_fetch";
 import type { BookSummary, SongList, BookIndex } from "@/scripts/types";
-import { prepackaged_book_urls } from "@/scripts/constants";
+import { branch, prepackaged_book_urls } from "@/scripts/constants";
 import { Preferences } from "@capacitor/preferences";
+import { Directory, Encoding, Filesystem } from "@capacitor/filesystem";
+import type { DownloadFileResult, FileInfo } from "@capacitor/filesystem";
+import { Capacitor } from "@capacitor/core";
+import { loaded } from "tone";
 
 async function getBookUrls() {
     const imported_book_urls = await Preferences.get({ key: "externalBooks" });
     return prepackaged_book_urls.concat(JSON.parse(imported_book_urls.value ?? "[]"));
+}
+
+async function download_book(to_download: string) {    
+    let url_segments = to_download.split("/");
+    let book = url_segments[url_segments.length - 1];
+
+    let book_summary = await fetchBookSummary(`https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/${book}`)
+    let ext = book_summary?.fileExtension;
+    let songs: SongList | null = await getSongMetaData(book);
+    let num_of_songs = Object.entries(songs as any).length;
+    Filesystem.downloadFile({
+        directory: Directory.Documents,
+        path: `${book}/songs.json`,
+        progress: false,
+        url: `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/${book}/songs.json`
+    })
+    Filesystem.downloadFile({
+        directory: Directory.Documents,
+        path: `${book}/index.json`,
+        progress: false,
+        url: `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/${book}/index.json`
+    }).then((result) => {
+        console.log(result.path);
+    }).catch(err => {
+        console.log("Ignored File: " + err);
+    });
+
+    var i = 0;
+    for(let song_number in songs) {
+        let url = `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/${book}/songs/${song_number}.${ext}`
+        Filesystem.downloadFile({
+            directory: Directory.Documents,
+            path: `${book}/songs/${song_number}.${ext}`,
+            progress: false,
+            url: url
+        }).then((result) => {
+            i++;
+            let download_progress = `${i/num_of_songs*100}%`;
+            console.log(download_progress);
+            if(i/num_of_songs >= 1) {
+
+                Filesystem.downloadFile({
+                    directory: Directory.Documents,
+                    path: `${book}/summary.json`,
+                    progress: false,
+                    url: `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/${book}/summary.json`
+                }).then((new_result: DownloadFileResult) => {
+                    console.log(`${book} has finished downloading.`);
+                    let summary_path: string = new_result.path || "";
+                    summary_path = summary_path.replace("/summary.json", "");
+
+                    // this is used for calling fetch on downloaded books
+                    let web_url = Capacitor.convertFileSrc(summary_path);
+                });
+            }
+        });
+    }
 }
 
 async function fetchBookSummary(url: string, options: RequestInit & { timeout?: number } = { timeout: 500 }) {
