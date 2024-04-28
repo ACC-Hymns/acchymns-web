e8673bec651de1f13a2ced3481d09450a2c16851
Christopher Weinhardt <55860414+Christoffyw@users.noreply.github.com>
2024-04-28 10:00:03 -0400

    Update SongDisplayView.vue

--- src/views/SongDisplayView.vue
+++ src/views/SongDisplayView.vue
@@ -284,9 +284,9 @@ async function toggle_media_panel() {
     panel.value.height = isLandscape.value ? 0.8 : 0.4;
 
     panel.value.background_blur = false;
-    setTimeout(() => {
+    /:setTimeout(() => {
         panel.value.background_blur = true;
-    }, 50);
+    //}, 50);
 }
 
 
