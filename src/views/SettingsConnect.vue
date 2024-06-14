0e89a90a0a105575769ce687bf77e7ca61e4bdcf
Christoffyw <weinhardtchristopher@gmail.com>
2024-06-14 01:52:31 -0400

    Revamp bible reading UI

--- src/views/SettingsConnect.vue
+++ src/views/SettingsConnect.vue
@@ -60,7 +60,15 @@ async function signout() {
 
 onMounted(async () => {
   bible.value = await fetchCachedJSON<Bible>("/NKJV.bible.json", {}) || { version: "", books: []};
-  console.log(bible.value);
+  
+  for(let [index, book] of bible.value.books.entries()) {
+    if(index > 38)
+      new_testament.value.push(book);
+    else
+      old_testament.value.push(book);
+  }
+  fix_chapter_range();
+  fix_verse_range();
 
   let broadcasting_auth_token = await Preferences.get({ key: "broadcasting_auth_token"});
   if (broadcasting_auth_token.value != "" && broadcasting_auth_token.value != undefined) {
@@ -91,12 +99,15 @@ async function broadcast(e: MouseEvent) {
 
     let top_text = "";
     let bottom_text = "";
-    if(read_type.value == 0) {
+    if(read_type.value == 1) {
       top_text = `${book.value}`;
       bottom_text = `${chapter_start.value}:${verse_start.value}-${bible.value?.books.find(b => b.name == book.value)?.chapters.find(c => c.num == chapter_start.value)?.verses.length}`;
-    } else if(read_type.value == 1) {
+    } else if(read_type.value == 2) {
       top_text = `${book.value}`;
-      bottom_text = `${chapter_start.value}:${verse_start.value}-${verse_end.value}`;
+      if(verse_start.value == verse_end.value)
+        bottom_text = `${chapter_start.value}:${verse_start.value}`;
+      else
+        bottom_text = `${chapter_start.value}:${verse_start.value}-${verse_end.value}`;
     } else {
       top_text = `${book.value} `;
       bottom_text = `${chapter_start.value}:${verse_start.value}-${bible.value?.books.find(b => b.name == book.value)?.chapters.find(c => c.num == chapter_start.value)?.verses.length}, ${chapter_end.value}:1-${verse_end.value}`;
@@ -107,29 +118,55 @@ async function broadcast(e: MouseEvent) {
 
 let bibleReading = ref<boolean>(false);
 
+let old_testament = ref<BibleBook[]>([]);
+let new_testament = ref<BibleBook[]>([]);
 let book = ref("Genesis");
 let chapter_start = ref(1);
 let chapter_end = ref(1);
 let verse_start = ref(1);
 let verse_end = ref(1);
-let read_type = ref(0);
+let read_type = ref(1);
+
+function fix_chapter_range() {
+  if(chapter_start.value > get_chapter_start_list(book.value).length) {
+    chapter_start.value = 1;
+  }
+  if(chapter_end.value > get_chapter_end_list(book.value).length) {
+    chapter_end.value = chapter_start.value;
+  }
+}
+
+function fix_verse_range() {
+  if(verse_start.value > get_verse_start_list(book.value, chapter_start.value).length) {
+    verse_start.value = 1;
+  }
+  if(verse_end.value > get_verse_end_list(book.value, (read_type.value == 3 ? chapter_end.value : chapter_start.value)).length) {
+    verse_end.value = (read_type.value == 3 ? 1 : verse_start.value);
+  }
+  if(read_type.value == 1)
+    verse_end.value = get_verse_start_list(book.value, chapter_start.value).length;
+}
 function book_changed(e: Event) {
-  book.value = (e.target as HTMLSelectElement).value;
+  book.value = (e.target as HTMLSelectElement).innerText;
+  fix_chapter_range();
+  fix_verse_range();
 }
 function chapter_start_changed(e: Event) {
-  chapter_start.value = Number.parseInt((e.target as HTMLSelectElement).value);
+  chapter_start.value = Number.parseInt((e.target as HTMLSelectElement).innerText);
+  fix_verse_range();
 }
 function chapter_end_changed(e: Event) {
-  chapter_end.value = Number.parseInt((e.target as HTMLSelectElement).value);
+  chapter_end.value = Number.parseInt((e.target as HTMLSelectElement).innerText);
+  fix_verse_range();
 }
 function verse_start_changed(e: Event) {
-  verse_start.value = Number.parseInt((e.target as HTMLSelectElement).value);
+  verse_start.value = Number.parseInt((e.target as HTMLSelectElement).innerText);
 }
 function verse_end_changed(e: Event) {
-  verse_end.value = Number.parseInt((e.target as HTMLSelectElement).value);
+  verse_end.value = Number.parseInt((e.target as HTMLSelectElement).innerText);
 }
 function read_type_changed(e: Event) {
-  read_type.value = (e.target as HTMLSelectElement).selectedIndex;
+  read_type.value = Number.parseInt((e.target as HTMLSelectElement).value);
 }
 
 function get_chapter_start_list(book: string) {
@@ -141,7 +178,7 @@ function get_chapter_end_list(book: string) {
   let b: BibleBook = bible.value?.books.find(b => b.name == book) || { name: "", chapters: []};
   let chapter_end_list: BibleChapter[] = [];
   if(chapter_start.value > 0) {
-    for (let i = chapter_start.value; i <= b?.chapters.length; i++) {
+    for (let i = chapter_start.value + 1; i <= b?.chapters.length; i++) {
       chapter_end_list.push(b.chapters[i-1]);
     }
     return chapter_end_list;
@@ -184,41 +221,79 @@ function get_verse_end_list(book: string, chapter: number) {
         </div>
         <div v-else-if="status == UserStatus.Authorized && bibleReading">            
           <div class="biblereading">
-              <label>Book
-                <select id="booklist" @change="(e: Event) => book_changed(e)">
-                  <option v-for="book in bible?.books" :value="book.name" :key="book.name">{{ book.name }}</option>
-                </select>
-              </label>
-              <div>
-                <label>Chapter
-                  <select id="chapterlist" @change="(e: Event) => chapter_start_changed(e)">
-                    <option v-for="c in get_chapter_start_list(book)" :value="c.num" :key="c.num">{{ c.num }}</option>
-                  </select>
-                </label>
-                <label> :
-                  <select id="verselist" @change="(e: Event) => verse_start_changed(e)">
-                    <option v-for="v in get_verse_start_list(book, chapter_start)" :value="v.num" :key="v.num">{{ v.num }}</option>
-                  </select>
-                </label>
-                <label> to
-                  <select id="verselist2" @change="(e: Event) => verse_end_changed(e)">
-                    <option v-for="v in get_verse_end_list(book, read_type == 2 ? chapter_end : chapter_start)" :value="v.num" :key="v.num">{{ v.num }}</option>
-                  </select>
-                </label>
+              <h3>Reading Type</h3>
+              <div class="book-selector">
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <option @click="(e) => read_type_changed(e)" class="biblebook" :class="{'selected': read_type == 1}" value="1">Start Only</option>
+                <option @click="(e) => read_type_changed(e)" class="biblebook" :class="{'selected': read_type == 2}" value="2">Start End</option>
+                <option @click="(e) => read_type_changed(e)" class="biblebook" :class="{'selected': read_type == 3}" value="3">Start End Chapter</option>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
               </div>
-
-              <label>Type
-                <select id="readtype" @change="(e: Event) => read_type_changed(e)">
-                  <option value="1">Start Only</option>
-                  <option value="2">Start End</option>
-                  <option value="3">Start End Chapter</option>
-                </select>
-              </label>
-              <label>End Chapter
-                <select id="chapterlist2" @change="(e: Event) => chapter_end_changed(e)">
-                  <option v-for="c in get_chapter_end_list(book)" :value="c.num" :key="c.num">{{ c.num }}</option>
-                </select>
-              </label>
+              <h3>Book</h3>
+              <div class="book-selector">
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook" :class="{'selected': b.name == book}" @click="(e: Event) => book_changed(e)" v-for="b in old_testament" :key="b.name">{{ b.name }}</a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+              </div>
+              <div class="book-selector">
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook" :class="{'selected': b.name == book}" @click="(e: Event) => book_changed(e)" v-for="b in new_testament" :key="b.name">{{ b.name }}</a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+              </div>
+              <h3>Chapter</h3>
+              <div class="number-selector">
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook" :class="{'selected': c.num == chapter_start}" @click="(e: Event) => chapter_start_changed(e)" v-for="c in get_chapter_start_list(book)" :key="c.num">{{ c.num }}</a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+              </div>
+              <h3>Start Verse</h3>
+              <div class="number-selector">
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook" :class="{'selected': v.num == verse_start}" @click="(e: Event) => verse_start_changed(e)" v-for="v in get_verse_start_list(book, chapter_start)" :key="v.num">{{ v.num }}</a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+              </div>
+              <h3 v-if="read_type > 1">End Verse</h3>
+              <div v-if="read_type > 1" class="number-selector">
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook" :class="{'selected': v.num == verse_end}" @click="(e: Event) => verse_end_changed(e)" v-for="v in get_verse_end_list(book, read_type == 2 ? chapter_end : chapter_start)" :key="v.num">{{ v.num }}</a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+              </div>
+              <h3 v-if="read_type == 3">End Chapter</h3>
+              <div v-if="read_type == 3" class="number-selector">
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook" :class="{'selected': c.num == chapter_end}" @click="(e: Event) => chapter_end_changed(e)" v-for="c in get_chapter_end_list(book)" :key="c.num">{{ c.num }}</a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+                <a class="biblebook space"></a>
+              </div>
               <div>
                 <button class="send-button" @click="(e) => broadcast(e)">Send</button>
               </div>
@@ -271,6 +346,74 @@ function get_verse_end_list(book: string, chapter: number) {
 label {
   font-size: 22px;
   color: var(--color);
+}
+
+.book-selector {
+  display: flex;
+  flex-wrap: nowrap; 
+  overflow: auto;
+  width: 95vw;
+
+  &:after {
+    content: "";
+    position: absolute;
+    z-index: 0;
+    top: 0;
+    right: 0;
+    bottom: 15px;
+    pointer-events: none;
+    background-image: linear-gradient(to right, rgba(255,255,255,0), var(--background) 85%);
+    width: 30px;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 15px;
+    pointer-events: none;
+    background-image: linear-gradient(to left, rgba(255,255,255,0), var(--background) 85%);
+    width: 30px;
+  }
+  scrollbar-width: none;  /* Firefox */
+}
+
+.number-selector {
+  display: flex;
+  flex-wrap: wrap; 
+  flex-direction: column;
+  overflow: auto;
+  width: 95vw;
+  height: 12vh;
+  scrollbar-width: none;  /* Firefox */
+}
+.book-selector::-webkit-scrollbar {
+  display: none;
+}
+.number-selector::-webkit-scrollbar {
+  display: none;
+}
+
+.biblebook {
+  flex: 0 0 auto;
+  vertical-align: middle;
+  background-color: lightgray;
+  padding: 5px 15px;
+  border-radius: 15px;
+  margin: 2px 2px 2px 2px;
+  line-height: 200%;
+  background-color: var(--search-color);
+  color: var(--color);
+  text-align: center;
+}
+.space {
+  opacity: 0;
+  width: 5px;
+}
+.selected {
+  -webkit-box-shadow:inset 0px 0px 0px 4px #2196F3;
+  -moz-box-shadow:inset 0px 0px 0px 4px #2196F3;
+  box-shadow:inset 0px 0px 0px 4px #2196F3;
 }
 
 .biblereading {
