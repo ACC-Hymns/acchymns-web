7ef2a585fcda801a992f2f3d87821e2bafe6f405
Christoffyw <weinhardtchristopher@gmail.com>
2024-06-28 11:27:11 -0400

    Add swipeback for iOS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import { setBackForwardNavigationGestures } from "capacitor-plugin-ios-webview-configurator";
 
 // Preload page-wide CSS and setup dark mode
 import "@/assets/css/globals.css";
@@ -16,6 +17,8 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
+// set webview settings
+setBackForwardNavigationGestures(true);
 
 import { registerConsoleStore } from "@/stores/console";
 registerConsoleStore();
