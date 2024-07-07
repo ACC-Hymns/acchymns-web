bc5eab21ca3b2b98d07cc9ea7a794e6757b3dc18
Christopher Weinhardt <weinhardtchristopher@gmail.com>
2024-07-07 14:41:34 -0400

    Update metadata

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,4 +1,4 @@
-const prepackaged_books = ["ZH", "GH", "JH"];//, "HG"];
+const prepackaged_books = ["ZH", "GH", "JH"];
 const prepackaged_book_urls: string[] = prepackaged_books.map(book_name => import.meta.env.BASE_URL + "books/" + book_name);
 
 const branch: string = import.meta.env.VITE_GIT_BRANCH;
@@ -12,6 +12,7 @@ const branch: string = import.meta.env.VITE_GIT_BRANCH;
 // }
 
 const public_references = {
+    HG: `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/HG`,
     CH: `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/CH`,
     //HSZ: `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/HSZ`,
     HZ: `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${branch}/public/books/HZ`,
